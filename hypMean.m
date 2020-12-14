ee7c2cc7a614c01b1b403d10ec84cc7a7cbb1f5e
Alexander Blyth <alexanderblyth5@gmail.com>
2020-12-14 17:55:22 +1000

    Provide more detail output for hypMean & hypMean2

---
+++ hypMean.m
@@ -0,0 +1,43 @@
+function result = hypMean(n, muBar, sd, hyp, test)
+    % HYPMEAN Test a mean alternate hypothesis against a null hypothesis
+    %
+    %   HYPMEAN(N, MUBAR, SD, HYP, TEST)
+    %   N = number of samples
+    %   MUBAR = sample calculated mean
+    %   SD = sample standard deviation of distribution
+    %   HYP = the current hypothesis (default 0)
+    %   TEST = hypothesis test we are conducting. One of 'equal', 'less' or 'upper'. (default 'equal')
+
+    if ~exist('hyp','var')
+        hyp = 0;
+    end
+    if ~exist('test','var')
+        test = 'equal';
+    end
+    tstat =  (muBar - hyp) / (sd / sqrt(n));
+
+    fprintf("standard error = s/sqrt(n) = %f/sqrt(%d) = %f\n", sd, n, sd/sqrt(n))
+    fprintf("test statistic = (estimate - hypothesised)/s.e.(estimate)\n")
+    fprintf("test statistic = (%f - %f)/%f = %f\n", muBar, hyp, sd/sqrt(n), tstat)
+    pval = 0;
+
+    switch test 
+    case 'equal'
+        pval = min(tcdf(tstat, n-1), tcdf(tstat, n-1, 'upper')) * 2;
+        fprintf("pval = 2 * min[P(Tn-1 > tstat), P(Tn-1 < tstat)]\n")
+        fprintf("pval = 2 * min[P(T%d > %f), P(T%d < %f)] = %f\n", n-1, tstat, n-1, tstat, pval)
+    case 'less'
+        pval = tcdf(tstat, n-1);
+        fprintf("pval = P(Tn-1 < tstat)\n")
+        fprintf("pval = P(T%d < %f) = %f\n", n-1, tstat, pval)
+    case 'greater'
+        pval = tcdf(tstat, n-1, 'upper');
+        fprintf("pval = P(Tn-1 > tstat)\n")
+        fprintf("pval = P(T%d > %f) = %f\n", n-1, tstat, pval)
+    otherwise
+        fprintf("test must be one of 'equal', 'less' or 'greater'\n")
+        return
+    end
+
+    result = estrength(pval);
+end
---
+++ hypMean2.m
@@ -0,0 +1,44 @@
+function result = hypMean2(n1, mu1, sd1, n2, mu2, sd2, hyp, test)
+    % HYPMEAN2 Test an alternate hypothesis against a null hypothesis in difference in means 
+    %
+    %   HYPMEAN2(N1, MUBAR1, SD1, N2, MUBAR2, SD2, HYP, TEST)
+    %   N1 = number of samples in first distribution
+    %   MUBAR1 = sample calculated mean in first distribution
+    %   SD1 = sample standard deviation of distribution in first distribution
+    %   N2 = number of samples in second distribution
+    %   MUBAR2 = sample calculated mean in second distribution
+    %   SD2 = sample standard deviation of distribution in second distribution
+    %   HYP = the current hypothesis (default 0)
+    %   TEST = hypothesis test we are conducting. One of 'equal', 'less' or 'upper'. (default 'equal')
+
+    if ~exist('hyp','var')
+        hyp = 0;
+    end
+    if ~exist('test','var')
+        test = 'equal';
+    end
+    poolsqrd  =  (((n1 - 1) * sd1^2) + (n2 - 1) * sd2^2)/ (n1 + n2 - 2);
+    fprintf("poolsqrd  =  (((n1 - 1) * sd1^2) + (n2 - 1) * sd2^2)/ (n1 + n2 - 2) = %f \n",poolsqrd)
+    
+    tstat =  ((mu1 - mu2) - hyp) / (sqrt(poolsqrd)*sqrt((1/n1) + (1/n2)));
+    fprintf("tstat =  ((mu1 - mu2) - hyp) / (sqrt(poolsqrd)*sqrt((1/n1) + (1/n2))) = %f \n", tstat)
+
+    switch test 
+    case 'equal'
+        pval = 2 * min(tcdf(tstat,n1+n2-2),tcdf(tstat,n1+n2-2,'upper'));
+        fprintf("pval = 2 * min[P(Tn+m-2 > tstat), P(Tn+m-2 < tstat)]\n")
+        fprintf("pval = 2 * min[P(T%d > %f), P(T%d < %f)] = %f\n", n1+n2-2, tstat, n1+n2-2, tstat, pval)
+    case 'less'
+        pval = tcdf(tstat,n1+n2-2);
+        fprintf("pval = P(Tn+m-2 < tstat)\n")
+        fprintf("pval = P(T%d < %f) = %f\n", n1+n2-2, tstat, pval)
+    case 'greater'
+        pval = tcdf(tstat,n1+n2-2,'upper');
+        fprintf("pval = P(Tn+m-2 > tstat)\n")
+        fprintf("pval = P(T%d > %f) = %f\n", n1+n2-2, tstat, pval)
+    otherwise
+        fprintf("test must be one of 'equal', 'less' or 'greater'\n")
+        return
+    end
+    result = estrength(pval);
+end
