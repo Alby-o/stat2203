f810fb7b730c8a3c0ad938650c854b0a84813c8c
Alexander Blyth <alexanderblyth5@gmail.com>
2020-12-15 14:44:45 +1000

    Improve output comments in ciMean

--- ciMean.m
+++ ciMean.m
@@ -1,19 +1,19 @@
-function ciMean(n, muBar, s, ci)
+function ciMean(n, xBar, s, ci)
     % CIMEAN Construct the confidence interval for an unknown mean with a unknown variance using student's t-distribution
     %
-    %   CIMEAN(n, muBar, sigma, ci)
+    %   CIMEAN(n, xBar, sigma, ci)
     %   n = number of samples
-    %   muBar = sample calucluated mean
+    %   xBar = sample calucluated mean
     %   sigma = sample standard deviation of distribution
     %   ci = the % confidence inteval to construct
     a = 1 - ci;
-    fprintf("a = 1 - ci = %f\n", a)
+    fprintf("%f = 1 - a => a = %f\n", ci, a)
     z = tinv(1 - (a/2), n-1);
-    fprintf("z = tinv(1-(a/2), n-1) = %f\n", z)
+    fprintf("t1-a/2;n-1 = t%f;%d = %f\n", 1-(a/2), n-1, z)
     
     serror = s / sqrt(n);
-    fprintf("err = s / sqrt(n) = %f\n", serror)
+    fprintf("s.e.(estimate) = s / sqrt(n) = %f\n", serror)
     
-    fprintf("CI is mean +- (z*err) = %f +- %f\n", muBar, z*serror)
-    fprintf("A range of [%f,%f]\n", (muBar - z*serror),(muBar + z*serror))
+    fprintf("CI is estimate +- critical value * s.e.(estimate) = %f +- %f\n", xBar, z*serror)
+    fprintf("A range of [%f,%f]\n", (xBar - z*serror), (xBar + z*serror))
 end
